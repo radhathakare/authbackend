d28727a3d04891378502f7a68262272fe5be5d2b
RADHA THAKARE <radhathakare@itantaanalytics.com>
2023-01-04 17:21:40 +0530

    changes done

--- db/db.js
+++ db/db.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://admin1ad:admin1@test.jh3hxyb.mongodb.net/Asystem?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  strictQuery: false
+  strictQuery: true
 });
